978e1e108271ad8597c6e4d6a328302f6c73c45c
Emily <emily.sear@cuw.edu>
2021-03-08 19:07:24 -0600

    changed stuff

--- driver.cpp
+++ driver.cpp
@@ -5,8 +5,31 @@
 
 using namespace std;
 
+void insertionSort(int ar[], int length);
+void displayIntArray(int ar[]);
+
+#define ARRAY_LENGTH 10 //called a macro 
+
 int main(int argc, char** argv)
 {
+    //this gives you actual random numbers --> you have to see the random number generator
+    srandom(time(NULL));
+
+    long val;
+    int ar[10];
+    cout << sizeof(ar) / sizeof(int) << endl; //how you find the acutal length of an array
+    for (int i  = 0; i < ARRAY_LENGTH; i++)
+    {
+        val = random();
+        val = val % 1000; //the numbers between 0 and 999
+        ar[i] = val;
+    }
+    displayIntArray(ar);
+    insertionSort(ar, ARRAY_LENGTH);
+    cout << endl;
+    displayIntArray(ar);
+   
+
     LinkedList* ll = new LinkedList();
     
     ll->addEnd(4);
@@ -20,4 +43,31 @@ int main(int argc, char** argv)
     ll->display();
 
     return 0;
+}
+
+//insertion sort with ints 
+void insertionSort(int ar[], int length)
+{
+    int tempSwap;
+    int theFollower;
+
+    for(int currStart = 0; currStart < length; currStart++)
+    {
+        theFollower = currStart;
+        while(theFollower > 0 && ar[theFollower] < ar[theFollower -1])
+        {
+            tempSwap = ar[theFollower];
+            ar[theFollower] = ar[theFollower - 1];
+            ar[theFollower - 1] = tempSwap;
+            theFollower--;
+        }
+    }
+}
+
+void displayIntArray(int ar[])
+{
+    for(int i = 0; i < ARRAY_LENGTH; i++)
+    {
+        cout << ar[i] << endl;
+    }
 }
